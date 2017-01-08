53a49511bfb8a258b278b1cc0123ca76af39117e
Stephanie Nemeth <traumverloren@gmail.com>
2017-01-08 09:42:35 +0100

    a little cleanup of commenting

--- instructions.js
+++ instructions.js
@@ -3,31 +3,33 @@ const fs = require("fs");
 const text = fs.readFileSync("./input.txt").toString('utf-8');
 const textArray = text.split('\n');
 
-// we'll store info about the bots,
-// which bots their high/lows go to,
-// and what values they currently have
+// we'll store info about the bots, which bots their high/lows go to,
+// and what values they currently have in an array
 let bots = [];
 
 // go through text lines and parse out the instructions that assign VALUES
-// and put that info into an object in the bots array
+// and put that info into an object in bots
 textArray.map(line => {
-  // find bot values and push to bots array
+  // finds lines starting with "value" and push info to bots
   if (line.match(/^value/)) {
     const matches = line.match(/(\d+)/g).map(parseFloat);
     const botId = matches[1];
     const value = matches[0];
 
+    // if a bot already in a bots with this botId, just update that object's values
     const foundBot = bots.filter(bot => (bot.botId === botId))[0];
     if (foundBot) {
       foundBot.values.push(value);
     } else {
+    // if bot isn't there already in bots, add a new bot object
       bots.push({
         botId,
         values: [value],
       });
     }
   }
-  // find bots high/low bot assignments and add/update objects to bots array
+  // finds lines starting with "bot"
+  // finds bots high/low bot assignments and add/update objects to bots
   if (line.match(/^bot/)) {
     const matches = line.match(/(\d+)/g).map(parseFloat);
     const botId = matches[0];
@@ -35,11 +37,13 @@ textArray.map(line => {
     const highDestination = { type: line.match(/(?:high to )(\w+)/)[1], id: matches[2] };
 
     const foundBot = bots.filter(bot => (bot.botId === botId))[0];
+    // if a bot already in a bots with this botId, just update that object's values
     if (foundBot) {
       foundBot.lowDestination = lowDestination;
       foundBot.highDestination = highDestination;
       foundBot.hasExchanged = false;
     } else {
+      // if bot isn't there already in bots, add a new bot object
       bots.push({
         botId,
         lowDestination,
@@ -51,7 +55,7 @@ textArray.map(line => {
   }
 })
 
-// filters out ones in the bots array with 2 values AND they haven't exchanged chips yet.
+// filters out ones in bots with 2 values AND they haven't exchanged chips yet.
 function hasEligibleBots(bot) {
   return (bot.values.length === 2 && bot.hasExchanged === false);
 }
@@ -68,12 +72,11 @@ function findBot(values) {
 
 // This runs recursively to keep exchanging chips until all bots with 2 chips have exchanged theirs.
 function exchangeChips() {
-  // find the bots with 2 microchips
+  // find the bots with 2 microchips & that haven't exchanged them yet
   const botsToRun = bots.filter(hasEligibleBots);
 
   if (botsToRun.length > 0) {
     for (const bot of botsToRun) {
-      bot.values.sort();
       // update the high destination values
       if (bot.highDestination.type === 'bot') {
         const high = bots.find(item => item.botId === bot.highDestination.id)
@@ -84,6 +87,7 @@ function exchangeChips() {
         const low = bots.find(item => item.botId === bot.lowDestination.id)
         low.values.push(Math.min(...bot.values));
       }
+      bot.values.sort();
       bot.hasExchanged = true;
     }
 
